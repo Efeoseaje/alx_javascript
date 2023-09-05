21302c6c1a33818a915cf0d9f5b790b972ce465c
Efeoseaje <efeoseaje@yahoo.com>
2023-09-05 22:00:14 +0100

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 // script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
-const myArray = ['C is fun', 'Pythin is cool', 'JavaScript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const element of myArray) {
   console.log(element);
