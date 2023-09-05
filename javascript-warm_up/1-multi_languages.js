d59f78ec742f07d18a649f6efacc81a4eef6fd78
Efeoseaje <efeoseaje@yahoo.com>
2023-09-05 21:44:42 +0100

    3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+// script that prints 3 lines
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'JavaScript is amazing';
+
+console.log(c);
+console.log(python);
+console.log(js);
