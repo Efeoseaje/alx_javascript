55dc3aa73de7a751e98c2912188020ad2b9b410c
Efeoseaje <efeoseaje@yahoo.com>
2023-09-09 03:01:54 +0100

    Add me maybe

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,5 @@
 // a function that increments and calls a function
 
-let newNumber;
 exports.addMeMaybe = function (number, theFunction) {
-  newNumber += number;
-  theFunction(newNumber);
+  theFunction(++number);
 };
