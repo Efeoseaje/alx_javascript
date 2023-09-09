afd360a0c72d675d3643a5111974de34b1c78075
Efeoseaje <efeoseaje@yahoo.com>
2023-09-09 15:19:40 +0100

    Square #1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,16 +5,12 @@
 const Square1 = require('./5-square');
 
 class Square extends Square1 {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
