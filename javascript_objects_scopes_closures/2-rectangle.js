2e4631bb3cec568c9bc2fcf78dfbd7c4366970fe
Efeoseaje <efeoseaje@yahoo.com>
2023-09-09 06:15:48 +0100

    Rectangle #2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+// class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
