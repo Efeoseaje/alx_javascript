5ca8826d5894fc32ed1b45c803e88a5f9ed63738
Efeoseaje <efeoseaje@yahoo.com>
2023-09-09 15:12:48 +0100

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// class Square that defines a square and inherits from Square
+
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
+      }
+    }
+  }
+}
+
+module.exports = Square;
