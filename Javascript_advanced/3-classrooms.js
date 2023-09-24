4e0b7d1ae5f00c7d862ae300386c397308e3b5c1
Efeoseaje <efeoseaje@yahoo.com>
2023-09-24 11:05:57 +0100

    Closure and loops

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const createClassRoom = (numberOfStudents) => {
+  const studentSeat = (seat) => {
+    return () => seat;
+  };
+
+  const students = [];
+
+  for (let i = 0; i <= numberOfStudents; i++) {
+    students.push(studentSeat(i + 1));
+  }
+  return students;
+};
+const classRoom = createClassRoom(10);
+
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
